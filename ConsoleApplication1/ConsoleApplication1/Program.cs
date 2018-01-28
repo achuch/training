ea3060adbe63632cc1184784f19283bfd1ea1ef9
DESKTOP-LPDU7FA\achuc <agatachucherko@gmail.com>
2018-01-28 18:20:31 +0100

    addidng models

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.Models;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yes");
+
+            Product newProduct = new Product()
+            {
+                ID = 1,
+                Category = "Narzędzia",
+                Name = "Młotek"
+            };
+
+            Client newClient = new Client()
+            {
+                Id = 1,
+                Email = "aa@aa.pl"
+            };
+        }
+    }
+}
